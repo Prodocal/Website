5b431bae7732766992746c7772fa4e45c231c475
ApramodkPFJ <Akash.PramodKumar@pilotflyingj.com>
2022-10-06 13:01:05 -0400

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: ['svelte3'],
+	extends: ['eslint:recommended', 'prettier', 'tailwindcss:recomended'],
+	plugins: ['svelte3', 'tailwindcss',],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	parserOptions: {
 		sourceType: 'module',

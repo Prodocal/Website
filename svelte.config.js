a45f91e6160d4f2dda48909d3fe007a5a0fd92e8
ApramodkPFJ <Akash.PramodKumar@pilotflyingj.com>
2022-10-06 12:44:35 -0400

    init

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,16 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
-
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: [
+		preprocess({
+			postcss: true,
+		})
+	]
 };
 
 export default config;

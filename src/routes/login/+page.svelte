b7640c4a995f3f8efa32227a7e573c2f5a72a6d9
apramodk <62346607+apramodk@users.noreply.github.com>
2022-10-21 12:14:50 -0400

    Ricky's login page
    Corrected to have Ricky's code for login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,1 +1,58 @@
+<html data-theme="dark"></html>
+
+<div id="pink_background">
+    <div id="divBox">
+    
+        <div class="card w-96 glass text-primary-content" id= "Alexyu">
+        <div class="card-body">
+        
+        <h2 class="card-title" style='color: black;font-size: 35px;'> Welcome</h2>
+        
+        <h2 class="card-title" style='color: black;font-size: 35px;'>Back </h2>
+        <br>
+
+        <label class="label">
+            <span class="label-text" style='color: black;'>Username</span>
+        </label>
+        <input type="text" placeholder="Type your username" class="input input-bordered w-full max-w-xs" />
+        <label class="label"> </label>
+
+
+        <label class="label">
+            <span class="label-text" style='color: black;'>Password</span>
+        </label>
+        <input type="password" placeholder="Type your password" class="input input-bordered w-full max-w-xs" />
+        <label class="label"> 
+            <span class="label-text-alt"></span>
+            <span class="label-text-alt" style='color: black;'>Forgot Password?</span>
+        </label>
+
+        <br>
+        <button class="btn btn-wide" style="background-color:neutral;"> Login </button>
+
+        </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    #divBox{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    #pink_background{
+        background-image: url("https://khaodesign.com/wp-content/uploads/2017/03/beautiful-color-gradients-backgrounds-158-angel-care.png");
+        background-size: cover;
+    }
+
+    #Alexyu{
+        opacity: 0.83;
+    }
+
+</style>
+
+
 

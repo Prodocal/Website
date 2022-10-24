9bffbbe6d3821675a9ad486de13b4d8f04f3e997
Ricky Lau <rlau@vols.utk.edu>
2022-10-24 02:47:51 -0400

    Calendar frontend revised

---
+++ src/routes/calendar2/+page.svelte
@@ -0,0 +1,162 @@
+
+<script>
+    // <script> is used to embed executable code or data. Usually refers to Javascript code. 
+    
+    const date = new Date(); // creates a Date object and represents the current date and time
+
+    // initialize an array full of all months for later use
+    // using the right and left arrows on the front page will move between months
+    const listofMonths = [ "January", "February", "March", "April", "May", "June", "July",  
+    "August", "September", "October", "November", "December" ];   
+    
+    let monthIndex = date.getMonth(); // .getMonth() returns the month number and acts as an index for our array
+
+    // let allows you to declare variables 
+    $: month = listofMonths[monthIndex]; // $: reactive symbol makes it update real time if we change the index
+    let year = date.getFullYear();
+
+    $: firstday = new Date(year, monthIndex, 1).getDay(); // gets the number on the calendar for what day is the first
+    
+    // 0 as the parameter gives the last day of last month, so to get the current month we +1 the month index
+    $: numdays = new Date(year, monthIndex+1, 0).getDate(); // so this returns the number of days in the month
+
+    let currentDay = date.getDate(); // gets the current day
+
+    const goPrevMonth = () => {
+        if (monthIndex <= 0){
+            year--;
+            return monthIndex = 11;
+        }
+
+        return monthIndex--;
+    }
+
+    const goNextMonth = () => {
+        if (monthIndex >= 11){
+            year++;
+            return monthIndex = 0;
+        }
+
+        return monthIndex++;
+    }
+                    
+
+    $: console.log(`${monthIndex} --- First Day Index: ${firstday}-- Number of Days: ${numdays} --- ${month} ${currentDay}`)
+
+</script>
+
+<main>
+    
+    <div class="month">
+    <ul>
+        <li class="prev" on:click={goPrevMonth}>&#10094;</li>
+        <li class="next" on:click={goNextMonth}>&#10095;</li>
+        <li> {month} <br><span style="font-size:18px"> {year} </span></li>
+    </ul>
+    </div>
+
+    <ul class="weekdays">
+        <li>Su</li>
+        <li>Mo</li>
+        <li>Tu</li>
+        <li>We</li>
+        <li>Th</li>
+        <li>Fr</li>
+        <li>Sa</li>
+    </ul>
+
+    <ul class="days">
+        {#each Array(42) as _, i}
+            {#if i < firstday || i >= numdays + firstday}
+                <li>&nbsp;</li>
+            {:else}
+                <li class:active={i === currentDay+(firstday-1)}> {i - firstday+1}  </li> 
+            {/if}
+        {/each}
+    </ul>
+
+</main>
+
+<style> /* style tag is used to define style information. Specifys how HTML elements should render in main. */
+
+    ul {list-style-type: none;}
+    main {font-family: Verdana, sans-serif;}
+
+    /* Month header */
+    .month {
+    padding: 70px 25px;
+    width: 100%;
+    background-image: url('../background/pink_image.png');
+    background-size: cover;
+    text-align: center;
+    }
+
+    /* Month list */
+    .month ul {
+    margin: 0;
+    padding: 0;
+    }
+
+    .month ul li {
+    color: white;
+    font-size: 20px;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+    }
+
+    /* Previous button inside month header */
+    .month .prev {
+    float: left;
+    padding-top: 10px;
+    cursor: pointer;
+    }
+
+    /* Next button */
+    .month .next {
+    float: right;
+    padding-top: 10px;
+    cursor: pointer;
+    
+    }
+
+    /* Weekdays (Mon-Sun) */
+    .weekdays {
+    margin: 0;
+    padding: 10px 0;
+    background-color:rgb(75, 74, 74);
+    }
+
+    .weekdays li {
+    display: inline-block;
+    width: 13.6%;
+    color: #ffffff;
+    text-align: center;
+    }
+
+    /* Days (1-31) */
+    .days {
+    padding: 10px 0;
+    background: #eee;
+    margin: 0;
+    }
+
+    .days li {
+    list-style-type: none;
+    display: inline-block;
+    border: 1px solid black;
+    padding: 35px;
+    width: 13.6%;
+    text-align: center;
+    margin-bottom: 5px;
+    font-size:12px;
+    color: #777;
+    }
+
+    /* Highlight the "current" day */
+    .active {
+    padding: 5px;
+    background: #3d3d3d2d;
+    color: white !important
+    }
+
+</style>

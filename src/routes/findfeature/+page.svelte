9b12fbe0a9f6b6bf85faa660fe9ade426e8a8260
kmlbyc <kamilbaycora@gmail.com>
2022-10-25 00:08:53 -0400

    completed... for now

--- src/routes/findfeature/+page.svelte
+++ src/routes/findfeature/+page.svelte
@@ -4,20 +4,10 @@
 </script>
 
 <main>
-    <div class="md:flex md:justify-center mb-6">
-        <div class="card w-9/12">
-            <div class="card-body">
-                <div class="relative">
-                    <input type="search" id="default-search" class="block p-4 pl-10 w-full text-lg text-[#FF899C] rounded-xl border-2 border-[#FF899C] placeholder-[#FF899C] hover:shadow-xl" placeholder="Find Friends & Organizations..." required>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="md:flex md:justify-center mb-6">
+    <div class="md:flex md:justify-center mb-6 mt-10">
         <div class="card bg-base-100 shadow-xl w-9/12 ">
-            <div class="card-body bg-gradient-to-r from-[#FFC99F] to-[#FF899C]">
-            <h1 class="text-white text-3xl text-left pl-6 font-extrabold">Recommended</h1>
+            <div class="card-body bg-gradient-to-r from-[#FAE1A7] to-[#FFAB9F]">
+            <h1 class="text-[#31302e] text-3xl text-left pl-6 font-extrabold">Your Friends & Organizations</h1>
             <Users />
             </div>
         </div>

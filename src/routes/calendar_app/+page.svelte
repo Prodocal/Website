ab460b7404492ce6b20e454cfe50b77a21badf34
Ricky Lau <rlau@vols.utk.edu>
2022-10-25 04:12:57 -0400

    Seperate page for calendar app and schedule page

---
+++ src/routes/calendar_app/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Calendar from '../calendar2/+page.svelte'
+</script>
+
+<main>
+    <Calendar/>
+   
+</main>
+
+
+<style>
+    main {font-family: Verdana, sans-serif}
+</style>

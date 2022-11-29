e2d295093b6fdbedaaaa45c4b9dc8bb8cff8c3e4
Mason Stott <mstott3@vols.utk.edu>
2022-11-28 19:24:37 -0500

    switching over to new calendar

---
+++ src/lib/components/calendar.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Calendar from '@event-calendar/core';
+  import TimeGrid from '@event-calendar/day-grid';
+
+  let plugins = [TimeGrid];
+  let options = {
+      view: 'dayGridMonth',
+      events: [
+          // your list of events
+      ]
+  };
+
+  
+</script>
+
+<Calendar {plugins} {options} />
--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -2,29 +2,17 @@
     import MonthView from "$lib/components/MonthView.svelte";
     import Month from "$lib/components/calendar/Month.svelte";
     import Create from "$lib/components/Create.svelte";
-
-
+    import Calendar from "$lib/components/calendar.svelte";
 </script>
 
 <main> 
     <div class="card card-side h-[85vh] w-[95vw] shadow-xl backdrop-blur bg-white/50">
-        <div class="card-body w-10 flex flex-col">
-            <div class="flex-1">
-                <Create/>
-            </div>
+        <div class="card-body w-[40px] flex-none">
+            <Create/>
         </div>
-        <Month/>        
+        <div class="card-body w-10 flex-auto">
+            <Calendar/>    
+        </div>    
     </div>
     
 </main>
-
-<style>
-    main {
-        padding: 10px;
-        font-family: Verdana, sans-serif;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-
-    }
-</style>

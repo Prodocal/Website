c70cb44925a7cdb4ea5b03b6d8ff9be8008f042d
Mason Stott <mstott3@vols.utk.edu>
2022-11-28 20:30:41 -0500

    Finished first pass of new calendar with event population

--- src/lib/components/calendar.svelte
+++ src/lib/components/calendar.svelte
@@ -2,6 +2,8 @@
   import Calendar from '@event-calendar/core';
   import TimeGrid from '@event-calendar/day-grid';
 
+  let cal_inst;
+  let tmp; // DEBUG
   let plugins = [TimeGrid];
   let options = {
       view: 'dayGridMonth',
@@ -10,7 +12,25 @@
       ]
   };
 
-  
+  // This function populates the calendar with the user's events on page load
+  function pop_events()
+  {
+    let event = {
+      id: 69,
+      allDay: true,
+      title: "Test Event"
+    }
+
+    tmp = cal_inst.addEvent(event);
+  }
+
+  function remove_test()
+  {
+    cal_inst.removeEventById(tmp.id);
+  }
+
 </script>
 
-<Calendar {plugins} {options} />
+<button on:click={pop_events}>add events</button>
+<button on:click={remove_test}>remove test event</button>
+<Calendar bind:this={cal_inst} {plugins} {options}/>

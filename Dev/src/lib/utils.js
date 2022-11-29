7f9d7c84d8e0ca10edb6380cf6c13ad11bb3cce0
ApramodkPFJ <Akash.PramodKumar@pilotflyingj.com>
2022-11-29 05:42:53 -0500

    done

--- Dev/src/lib/utils.js
+++ Dev/src/lib/utils.js
@@ -1,4 +1,5 @@
 const { randomBytes } = await import('node:crypto');
+// global.structuredClone = (val) => JSON.parse(JSON.stringify(val));
 
 export const serializeNonPOJOs = (obj) => {
 	return structuredClone(obj);

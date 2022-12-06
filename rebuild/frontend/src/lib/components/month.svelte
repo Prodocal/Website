ba9dd7af8665d5fd07842e0609f0022268b619c6
ApramodkPFJ <Akash.PramodKumar@pilotflyingj.com>
2022-12-05 22:53:27 -0500

    saved

--- rebuild/frontend/src/lib/components/month.svelte
+++ rebuild/frontend/src/lib/components/month.svelte
@@ -22,25 +22,46 @@
         "November", 
         "December",
     ];
+    let daysThisMonth = [
+        1,2,3,4,5,6,7,
+        1,2,3,4,5,6,7,
+        1,2,3,4,5,6,7,
+        1,2,3,4,5,6,7,
+        1,2,3,4,5,6,7,
+    ]
 
-    let now = new Date();
+    let date = new Date();
+    let month = date.toLocaleDateString('en-US', {month: 'long'})
+    let year = date.getFullYear();
 </script>
 
 <main class="flex flex-col justify-center items-center">
-    <div class="navbar bg-base-200 mb-5">
-        <div class="btn btn-ghost">
+    <div class="navbar bg-base-200 mb-5 flex justify-center items-center">
+        <div class="flex-1 w-5 btn btn-ghost">
             prev
         </div>
-        Current Date
-        <div class="btn btn-ghost">
+        
+        <h1 class="flex-2 text-2xl text-bold">
+            {month}
+        </h1>
+
+        <div class="flex-1 btn w-5 btn-ghost">
             next
         </div>
     </div>
-    <div class="grid grid-cols-7 grid-rows-4 gap-2">
+
+    <!-- days of the week -->
+    <div class="grid grid-cols-7 grid-rows-1 gap-2 pb-5">
         {#each days as day}
         <div class='card w-[10vw] bg-base-200 flex items-center justify-center'>{day}</div>
         {/each}
     </div>
-    { now }
+
+    <!-- days in the month -->
+    <div class="grid grid-cols-7 grid-rows-5 gap-2">
+        {#each daysThisMonth as day}
+            <div class='card w-[10vw] h-36 bg-base-200 flex items-center justify-center'>{day}</div>
+        {/each}
+    </div>
 
 </main>

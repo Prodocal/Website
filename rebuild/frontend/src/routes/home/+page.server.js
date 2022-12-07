e18cf0c3a0bf09a7e26b8929b2a6b61cf3861651
ApramodkPFJ <Akash.PramodKumar@pilotflyingj.com>
2022-12-06 20:54:29 -0500

    updated pocketbase rules

--- rebuild/frontend/src/routes/home/+page.server.js
+++ rebuild/frontend/src/routes/home/+page.server.js
@@ -3,15 +3,20 @@ import { error, redirect } from '@sveltejs/kit';
 export const actions = {
 	addEvent: async ({ request, locals }) => {
         const body = Object.fromEntries(await request.formData());
+
+        let owningUser = locals.user.id;
+
         try {
-            await locals.pb.collection('events').create({...body});
+            await locals.pb.collection('events').create({owningUser, ...body});
         }
         catch(err) {
             console.log('Error: ', err);
-            throw error(500, 'Something went wrong')
+            throw error(500, 'Something went wrong');
         }
-        console.log(locals.user.username);
-        console.log(body);
+        
+
+
+        console.log(locals);
 	},
     editEvent: async ({request, locals}) => {
         const body = Object.fromEntries(await request. formData());
